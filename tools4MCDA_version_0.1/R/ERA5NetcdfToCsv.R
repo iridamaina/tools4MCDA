3d7b4712bfef40ca30204d4ed13083985ad8302a
iridamaina <imaina@hcmr.gr>
2025-06-02 13:52:02 +0300

    Update ERA5NetcdfToCsv.R
    Update example

--- tools4MCDA_version_0.1/R/ERA5NetcdfToCsv.R
+++ tools4MCDA_version_0.1/R/ERA5NetcdfToCsv.R
@@ -15,7 +15,6 @@
 #'   # Example 
 #'   library(ncdf4) # package for netcdf manipulation
 #'   library(raster) # package for raster manipulation
-#'   library(rgdal) # package for geospatial analysis
 #'   library(ggplot2) 
 #'   library(RNetCDF)
 #'
